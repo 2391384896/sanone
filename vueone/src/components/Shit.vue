f4bd40a522557af3922fd82c2781eb04fedaa836
DESKTOP-5TBM399\lx <1829088060@qq.com>
2019-10-10 15:51:07 +0800

    更新4

---
+++ vueone/src/components/Shit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="shit">
+    <fieldset>
+      <legend><h1>shit</h1></legend>
+      <p>我来自山川啊，要取你的头；我来自无人居住的将军府啊，要取你的头，</p>
+    </fieldset>
+  </div>
+
+</template>
+<script>
+    export default {
+        name: "Shit"
+    }
+</script>
+
+<style scoped>
+.shit{
+  width: 100%;
+  height: 100%;
+  background: darkorchid;
+}
+</style>
