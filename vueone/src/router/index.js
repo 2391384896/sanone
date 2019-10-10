ef4bbbf2ddd32479e825b7f2bd06761cf8b083a6
tianyonggen <1368833693>
2019-10-10 16:43:04 +0800

    8

--- vueone/src/router/index.js
+++ vueone/src/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Lphone from '../components/Lphone'
 import Shit from '@/components/Shit'
-
+import Tyg from '../components/Tyg'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:"",redirect:"/lphone"},
     {path:"/lphone",component:Lphone},
-    {path:"/shit",component:Shit}
+    {path:"/shit",component:Shit},
+    {path:"/Tyg",component:Tyg}
   ]
 })
